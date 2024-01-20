f19fd048a9f81e6114f12969d0dbcbeee66a3776
Lisa-cai <145958405+Lisa-cai@users.noreply.github.com>
2024-01-19 23:47:37 -0800

    Add project files.

---
+++ GA_ThisKeyword_Hongfei/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_ThisKeyword_Hongfei
+{
+    public class Person
+    {
+        //Add field 
+        private string name;
+        private int age;
+        private int id;
+
+        public string Name { get => name; set => name=value; }
+        public int Age { get => age; set => age=value; }
+        public int Id { get => id; set => id=value; }
+
+        //Constructor,use 'this' to distinguish fields from parameters
+        public Person(string name, int age, int id)
+        {
+            this.name = name;
+            this.age = age;
+            this.id=id;
+        }
+
+        //Overloaded constructor
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        //Method to display the person information
+
+        public void Display()
+        {
+            Console.WriteLine($" Name: {this.name}, Age: {this.age}, ID: {this.id}");
+        }
+
+        //Method to updated the person'age
+        public void SetAgeandID(int age,int id)
+        {
+            this.age = age;
+            this.id = id;
+        }
+
+    }
+}
---
+++ GA_ThisKeyword_Hongfei/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GA_ThisKeyword_Hongfei
+{
+    //Week2_GA_ThisKeyword
+    //Hongfei_C
+    //01/19/2024
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Sam", 10, 1001);
+            person1.Display();
+
+            Person person2 = new Person("Tina");
+            person2.Display();
+            person2.SetAgeandID(5,1002);
+            person2.Display();
+            Person person3 = person2;
+            person3.Name = "Laura";
+            person3.Id = 1003;
+            person3.Display();
+
+            Console.ReadKey();
+
+        }
+    }
+}
